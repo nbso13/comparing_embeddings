68206d74b00243a3b94d27da15b802512893617c
Bradley <bradleygoldsmith14@gmail.com>
2020-12-11 19:25:30 -0600

    testvecs

--- fasttext_bradley/for_nick.py
+++ fasttext_bradley/for_nick.py
@@ -1,9 +1,24 @@
 import fasttext as ft
-import fasttext_models as fm
 import export_wordvecs_fasttext as ev
+import numpy as np
+import get_accuracy as ga
 
-mcs,mus = ([mc0,mc1,mc2,mc3,mc4],[mu0,mu1,mu2,mu3,mu4]) = fm.main('no arg')
+mc1 = ft.load_model('data/fasttext_skipgram_cleaned_D50.bin')
+
 
 # the model we care about it mc1
 labels, reviews, vectors = ev.get_vectors(mc1)
-print(vectors)
+
+a,lables,reviews = ga.get_accuracy(mc1)
+
+test_vecs = []
+for r in reviews :
+    test_vecs.append(mc1.get_sentence_vector(r))
+
+
+
+test_vecs = np.stack(test_vecs, axis=0)
+vectors = np.stack(vectors, axis=0)
+
+np.save('fasttext_vectors',vectors)
+np.save('fasttext_test_vectors',test_vecs)
